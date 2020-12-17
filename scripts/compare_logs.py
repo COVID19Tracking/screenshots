1eb87cf873b9b88aa62ce4f17d920d8a0dfdb08c
Julia K <julia326@gmail.com>
2020-12-17 16:34:57 -0500

    adding one more log line to comparison script

--- scripts/compare_logs.py
+++ scripts/compare_logs.py
@@ -1,4 +1,9 @@
-""" Script comparing dry run logs between old screenshots and those from this repo. """
+""" Script comparing dry run logs between old screenshots and those from this repo.
+
+This assumes that both old and new screenshots have been run in dry-run mode, for each of the
+3 teams, and the paths are saved to what's seen below in the main. Note: this is meant as a
+ONE-TIME MIGRATION SCRIPT, not meant for future use as is.
+"""
 
 import json
 import re
@@ -11,7 +16,8 @@ def make_request_dict(dry_run_lines):
     for line in dry_run_lines:
         relevant_bit = line.split('Dry run: ')[1]
         if relevant_bit.startswith('Downloading'):
-            state, suffix, url = re.search('Downloading (.+?) (.+?) file from (.+?)$', relevant_bit).groups()
+            state, suffix, url = re.search(
+                'Downloading (.+?) (.+?) file from (.+?)$', relevant_bit).groups()
             request_dict[state][suffix] = 'file ' + url
         else:
             state, suffix, url, request = re.search(
@@ -38,12 +44,13 @@ def compare_screenshot_logs(old_log, new_log):
     print('Screenshots exist for %d states' % len(new_request_dict))
     
     for state in new_request_dict:
-        assert new_request_dict[state].keys() == old_request_dict[state].keys()  # same type of screenshots
+        assert new_request_dict[state].keys() == old_request_dict[state].keys()
         for screenshot_type in new_request_dict[state].keys():
             new_config = new_request_dict[state][screenshot_type]
             old_config = old_request_dict[state][screenshot_type]
             if new_config != old_config:
                 print(f'Doublecheck {state} {screenshot_type}:\nOld: {old_config}\nNew: {new_config}')
+    print('No more error messages for this comparison')
 
 
 if __name__ == '__main__':
