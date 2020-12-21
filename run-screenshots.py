59bdee0718608ab0d406c0e281714965313b3bcc
Julia K <julia326@gmail.com>
2020-12-21 13:06:49 -0500

    adding vaccine config for US: CDC tracker

--- run-screenshots.py
+++ run-screenshots.py
@@ -18,7 +18,7 @@ _ALL_STATES = [
     'AK', 'AL', 'AR', 'AS', 'AZ', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL', 'GA', 'GU', 'HI', 'IA', 'ID',
     'IL', 'IN', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME', 'MI', 'MN', 'MO', 'MP', 'MS', 'MT', 'NC', 'ND',
     'NE', 'NH', 'NJ', 'NM', 'NV', 'NY', 'OH', 'OK', 'OR', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX',
-    'UT', 'VA', 'VI', 'VT', 'WA', 'WI', 'WV', 'WY']
+    'UT', 'VA', 'VI', 'VT', 'WA', 'WI', 'WV', 'WY', 'US']
 
 
 def states_from_args(args):
